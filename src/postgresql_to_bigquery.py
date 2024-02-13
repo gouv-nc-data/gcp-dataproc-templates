449f5b80d2bb823cfc0d4c533f751ddd2118cc64
thomasguyader <thomas.guyader@gouv.nc>
2024-02-13 00:35:03 +0000

    fix(spark): correction nom fonction startswith

--- src/postgresql_to_bigquery.py
+++ src/postgresql_to_bigquery.py
@@ -27,9 +27,9 @@ def upload_table(spark: SparkSession, table_name: str, url: str, dataset: str, m
     get_logger(spark).info("###############################################")
     get_logger(spark).info(df.dtypes)
     get_logger(spark).info("###############################################")
-    import pyspark.sql.functions as F
+
     for c_name, c_type in df.dtypes:
-        if c_type.startsWith('decimal'):
+        if c_type.startswith('decimal'):
             get_logger(spark).info("conversion de decimal vers float")
             df = df.selectExpr("cast(%s as float) %s" % (c_name, c_name))
     get_logger(spark).info("upload de la table %s" % table_name['table_name'])
