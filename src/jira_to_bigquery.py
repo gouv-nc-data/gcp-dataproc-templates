8475a557513b2101f0000c5561ce5475376c6d3d
abugeia <antoine.bugeia@gouv.nc>
2025-02-27 16:18:38 +1100

    BREAKING CHANGE(jira2bq): refactor pour correction bugs

--- src/jira_to_bigquery.py
+++ src/jira_to_bigquery.py
@@ -5,7 +5,6 @@ import argparse
 import pytz
 
 from jira import JIRA
-import pandas as pd
 import unicodedata
 from datetime import datetime
 
@@ -26,40 +25,37 @@ JIRA_URL = "https://jira.gouv.nc"
 # jira_project = "" # ex PSPC
 # gcp_project = "" # ex prj-davar-p-bq-a01c
 # bq_dataset = "" # ex pspc
+# jira_token = ""
+
+CRED_FILE = '' # ex: prj-davar-p-bq-a01c-c04f4c56dc89.json
+
+jira_api_date_pattern = re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+\+1100")
 
 #----------------------------
-# auth
+# auth & logging
 #----------------------------
-# jira_token = ""
-# GCP
-CRED_FILE = '' # ex: prj-davar-p-bq-a01c-c04f4c56dc89.json
+
 ENV_LOCAL = os.path.exists(CRED_FILE)
 # Authenticate google drive and bigquery APIs using credentials.
 SCOPES = [ 'https://www.googleapis.com/auth/bigquery']
 
-if ENV_LOCAL:
-    creds = service_account.Credentials.from_service_account_file(CRED_FILE)
-else:
-    creds, _ = google.auth.default()
+try:
+    if ENV_LOCAL:
+        logging.basicConfig(level=logging.DEBUG)
+        creds = service_account.Credentials.from_service_account_file(CRED_FILE)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        log_client = google.cloud.logging.Client()
+        log_client.setup_logging()
 
-# retrive credentials for scopes defined.
-scoped_credentials = creds.with_scopes(SCOPES)
+        creds, _ = google.auth.default()
 
-#----------------------------
-# logging
-#----------------------------
+    # retrive credentials for scopes defined. google.auth.default() doesn't support methode if exex in an env without serv account
+    scoped_credentials = creds.with_scopes(SCOPES)
 
-if not ENV_LOCAL:
-    logging.basicConfig(level=logging.INFO)
-    log_client = google.cloud.logging.Client()
-
-    # Retrieves a Cloud Logging handler based on the environment
-    # you're running in and integrates the handler with the
-    # Python logging module. By default this captures all logs
-    # at INFO level and higher
-    log_client.setup_logging()
-else:
-    logging.basicConfig(level=logging.DEBUG) # NOTSET DEBUG INFO WARNING ERROR CRITICAL
+except Exception as e:
+    logging.error(f"Échec de l'authentification : {e}")
+    raise
 
 #----------------------------
 # Fonctions
@@ -127,7 +123,7 @@ def to_bigquery_name(field_name):
     # S'assurer que le nom commence par une lettre ou un underscore
     if not clean_name[0].isalpha() and clean_name[0] != '_':
         clean_name = '_' + clean_name
-    # Limiter la longueur du nom à 128 caractères, comme requis par BigQuery
+    # Limiter la longueur du nom à 128 caractères
     return clean_name[:128]
 
 def get_fields_map(jira):
@@ -168,51 +164,46 @@ def cast_value(value):
     return value
 
 
-def load_to_bq(client, issues, table_name, gcp_project, bq_dataset, schema): 
+def load_to_bq(client, issues, table_name, gcp_project, bq_dataset): 
 
     destination = f"{gcp_project}.{bq_dataset}.{table_name}"
 
-
-    if schema is None:
-        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE",
+    job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE",
                                         autodetect = True
-                                        )
-    else:
-        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE",
-                                            schema = schema
-                                            )
-        
+                                        )   
     job = client.load_table_from_json(issues, destination, job_config=job_config)
 
     job.result()  # Attendre la fin du job
 
     logging.info("Data for issue type '%s' have been insered successfully.", table_name)
 
-def reformat_timestamp(timestamp):
-    """
-    Reformat a date for BigQuery (YYYY-MM-DD HH:MM:SS.ssssss).
-    """
-    if not timestamp:
-        return None
+def convert_to_utc(date_str):
+    """Convertit une date ISO 8601 en UTC"""
     try:
-        # Tente de parser différents formats de timestamp
-        dt = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f%z")  # Avec timezone
-        tz = pytz.timezone("UTC")  # Adaptez si nécessaire
-        dt = dt.astimezone(tz)
-        return dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+        dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+        dt_utc = dt.astimezone(pytz.utc)  # Convertir en UTC
+        return dt_utc.strftime("%Y-%m-%d %H:%M:%S.%f")  # Format BigQuery
     except ValueError:
-        logging.error(f"Error : can't convert {timestamp}")
-        return None
+        return date_str
 
-def format_issues(issues, field_mapping, schema, exclusion_fields=[]):
+def traverse_and_convert(data, pattern):
+    """Parcourt un JSON et convertit toutes les dates détectées en UTC"""
+    if isinstance(data, dict):
+        return {key: traverse_and_convert(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [traverse_and_convert(item) for item in data]
+    elif isinstance(data, str) and pattern.match(data):
+        return convert_to_utc(data)
+    else:
+        return data  
+
+
+def format_issues(issues, field_mapping, exclusion_fields=[]):
 
     issues = [{field_mapping.get(k, k): v for k, v in item.items()} for item in issues]
 
     if len(exclusion_fields) != 0:
         issues = [{k: v for k, v in item.items() if k not in exclusion_fields} for item in issues]
-
-    if schema:
-        issues = format_parser(issues, schema)
 
     return issues
 
@@ -225,45 +216,21 @@ def clean_empty_structs(data):
     else:
         return data
 
-# def create_df(issues, field_mapping):
-#         data = []
-#         for issue in issues:
-#             row = {}
-#             row['jira'] = issue.key
-#             for key, value in issue.fields.__dict__.items():
-#                 if key not in exclusion_fields:
-#                     if key != 'parent':
-#                         row_key = field_mapping[key]
-#                     else:
-#                         row_key = key
-#                     row_value = cast_value(value)
+def merge_tables(bq_client, target, source, gcp_project, bq_dataset):
 
-#                     row[row_key] = row_value
-
-#             data.append(row)
-                
-#         return pd.DataFrame(data)
-
-def merge_tables(bq_client, target, source, gcp_project, bq_dataset, union_table):
-    delete_query  = f"""
-    DELETE FROM `{gcp_project}.{bq_dataset}.{target}`
-    WHERE jira IN (
-    {union_table}
-    )
-    """
-    bq_client.query(delete_query )
     insert_query = f"""
     INSERT INTO `{gcp_project}.{bq_dataset}.{target}`
     SELECT * FROM `{gcp_project}.{bq_dataset}.{source}`
     """
     bq_client.query(insert_query)
+    logging.info(f"Tempory table {source} insered in {target}.")
 
     bq_client.delete_table(f'{gcp_project}.{bq_dataset}.{source}')
     logging.info(f"Tempory table {source} deleted.")
 
-def get_last_update_date(bq_client, table_name, gcp_project, bq_dataset):
+def get_last_update_bq(bq_client, tables_names, gcp_project, bq_dataset):
     try:
-        query = f"SELECT max(Mise_a_jour) last_update FROM `{gcp_project}.{bq_dataset}.{table_name}`"
+        query = query_date_constructor(tables_names, gcp_project, bq_dataset)
         query_job = bq_client.query(query)
         
         for row in query_job.result():
@@ -273,9 +240,12 @@ def get_last_update_date(bq_client, table_name, gcp_project, bq_dataset):
             if isinstance(last_update, str):
                 # Si c'est une chaîne, convertir en objet datetime
                 last_update = datetime.strptime(last_update, '%Y-%m-%dT%H:%M:%S.%f%z')
-            
+
+            noumea_tz = pytz.timezone("Pacific/Noumea")
+            last_update_noumea = last_update.astimezone(noumea_tz)
+
             # Convertir au format Jira
-            jira_format_date = last_update.strftime('%Y/%m/%d %H:%M')
+            jira_format_date = last_update_noumea.strftime('%Y/%m/%d %H:%M')
             logging.debug(f"Formated date for Jira: {jira_format_date}")
         else:
             logging.warning("No date gathered from BigQuery.")
@@ -295,42 +265,32 @@ def string_to_list(string_data):
     else:
         return [item.strip().strip("'") for item in string_data.split(',')]
     
-def get_bigquery_schema(bq_client, table_name, gcp_project, bq_dataset):
-    table_id = f"{gcp_project}.{bq_dataset}.{table_name}"
-    table = bq_client.get_table(table_id)
-    return table.schema
+def query_date_constructor(tables_names, gcp_project, bq_dataset):
+    query_from = f"SELECT Mise_a_jour \n      FROM `{gcp_project}.{bq_dataset}.{tables_names[0]}`"
+    for table_name in tables_names[1:]:
+        query_from = f"{query_from} \n      UNION ALL SELECT Mise_a_jour FROM `{gcp_project}.{bq_dataset}.{table_name}`"
 
-def format_parser(issues, schema_fields):
-    for field in schema_fields:
-        if field.field_type == 'TIMESTAMP':
-            for issue in issues:
-                # print(f"field : {field.name}")
-                issue[field.name] = reformat_timestamp(issue[field.name])
-                
-        elif field.field_type == 'RECORD':
-            for issue in issues:
-                # Vérifier que la clé existe avant de descendre dans le RECORD
-                if field.name in issue and isinstance(issue[field.name], list):
-                    issue[field.name] = format_parser(issue[field.name], field.fields)
+    query =f"""
+    SELECT MAX(Mise_a_jour) last_update
+    FROM (
+      {query_from}
+      )
+    """
+    return query
 
-                elif field.name in issue and isinstance(issue[field.name], dict):
-                    issue[field.name] = format_parser([issue[field.name]], field.fields)[0]
-    return issues
+def rm_updated_jira(bq_client, tables_names, source, gcp_project, bq_dataset):
+    delete_query = ""
 
-def create_union_table(issues_features, gcp_project, bq_dataset):
+    for table_name in tables_names:
+        delete_query += f"""
+DELETE FROM `{gcp_project}.{bq_dataset}.{table_name}`
+WHERE jira IN (SELECT jira FROM `{gcp_project}.{bq_dataset}.{source}`);
+        """
 
-    first_key = True
-
-    for issue_type in issues_features.keys():
-        if issues_features[issue_type]['len'] != 0:
-
-            if first_key:
-                first_key = False
-                union_table = f"SELECT jira FROM `{gcp_project}.{bq_dataset}.{issues_features[issue_type]['table_name']}`"
-            else:
-                union_table = f"{union_table} UNION DISTINCT SELECT jira FROM `{gcp_project}.{bq_dataset}.{issues_features[issue_type]['table_name']}`"
-
-    return union_table
+    query_job = bq_client.query(delete_query)
+    query_job.result()
+    bq_client.delete_table(f'{gcp_project}.{bq_dataset}.{source}')
+    logging.info(f"Tempory table {source} deleted.")
 
 #----------------------------
 # Fonction principale
@@ -348,74 +308,54 @@ def jira_to_bq(jira_project, gcp_project, bq_dataset, jira_token, exclusion_fiel
     exclusion_fields = string_to_list(exclusion_fields)
     exclusion_types = string_to_list(exclusion_types)
 
-    issues_features = dict()
+    pj_issues_types_names = [issue_type.name for issue_type in pj_issues_types]
+    pj_issues_types_tables_names = [f"{to_bigquery_name(issue_type.name)}{'_subtask' if issue_type.subtask else ''}"  for issue_type in pj_issues_types]
+    
+    jira_format_date = get_last_update_bq(bq_client, pj_issues_types_tables_names, gcp_project, bq_dataset)
 
-    for issue_type in pj_issues_types:
+    jql_query = f'project={jira_project}'
 
-        if issue_type.name not in exclusion_types:
+    if jira_format_date:
+        jql_query = jql_query +  f' AND updated > "{jira_format_date}"'
 
-            logging.info("Treatment of  %s", issue_type.name)
-            table_name = to_bigquery_name(issue_type.name)
+    try:
+        issues = fetch_all_issues(jira, jql_query)
+    except Exception as e:
+        logging.error(f"Erreur lors de la récupération des issues: {e}")
+        
 
-            if issue_type.subtask:
-                table_name = table_name + '_subtask'
+    issues = traverse_and_convert(issues, jira_api_date_pattern)
 
-            jira_format_date = get_last_update_date(bq_client, table_name, gcp_project, bq_dataset)
-            
-            jql_query = f'project={jira_project} AND issuetype="{issue_type.name}"'
-            
-            schema = None
-            
-            # si on a une date d'update on traite le différentiel, sinon on crée une table avec toutes les issues
-            if jira_format_date:
-                jql_query = jql_query +  f' AND updated >= "{jira_format_date}"'
-                initial_table = table_name
-                table_name = '_tmp_' + table_name
+    changed_issues = None
 
-                schema = get_bigquery_schema(bq_client, initial_table, gcp_project, bq_dataset)
-            
-            try:    
-                issues = fetch_all_issues(jira, jql_query)
-                
-                print(len(issues))
+    if jira_format_date:
+        changed_issues = [{'jira':issue['jira']} for issue in issues]
+        
+    if jira_format_date:
+        load_to_bq(bq_client, changed_issues, '_keys_to_rm_', gcp_project, bq_dataset)
+        rm_updated_jira(bq_client, pj_issues_types_tables_names, '_keys_to_rm_', gcp_project, bq_dataset)
+    
+    for issue_name, issue_table_name_bq in zip(pj_issues_types_names, pj_issues_types_tables_names):
 
+        if issue_name not in exclusion_types:
 
-                issues_features[issue_type] = {
-                    'table_name' : table_name,
-                    'jira_format_date' : jira_format_date,
-                    'len' : len(issues)
-                    }
-                
+            logging.info("Treatment of  %s", issue_name)
+            issue_type_list = [issue for issue in issues if issue['issuetype']['name'] == issue_name]
+
+            if len(issue_type_list) != 0:
                 if jira_format_date:
-                    issues_features[issue_type]['initial_table'] = initial_table
-                else:
-                    issues_features[issue_type]['initial_table'] = table_name
+                    initial_table = issue_table_name_bq
+                    issue_table_name_bq = '_tmp_' + issue_table_name_bq
+                    # schema = get_bigquery_schema(bq_client, initial_table, gcp_project, bq_dataset)
 
+                issue_type_list = format_issues(issue_type_list, field_mapping, exclusion_fields)
+                issue_type_list = clean_empty_structs(issue_type_list)
+                load_to_bq(bq_client, issue_type_list, issue_table_name_bq, gcp_project, bq_dataset)
 
-                issues = format_issues(issues, field_mapping, schema, exclusion_fields)
-                issues = clean_empty_structs(issues)
-
-                if len(issues) != 0:
-                    load_to_bq(bq_client, issues, table_name, gcp_project, bq_dataset, schema)
-
-            except Exception as e:
-                logging.error(f"Erreur lors du traitement de l'issue type {issue_type.name}: {e}")
-                continue
-
-    # traitement du différentiel
-    union_table = create_union_table(issues_features, gcp_project, bq_dataset)
-
-    for issue_type in pj_issues_types:      
-
-        if issues_features[issue_type]['jira_format_date'] is not None and issues_features[issue_type]['len'] != 0:
-            merge_tables(bq_client,
-                        issues_features[issue_type]['initial_table'],
-                        issues_features[issue_type]['table_name'],
-                        gcp_project,
-                        bq_dataset,
-                        union_table)
-
-
+                if jira_format_date:
+                    merge_tables(bq_client, initial_table, issue_table_name_bq, gcp_project, bq_dataset)
+            logging.info("End of treatment of  %s", issue_name)
+    
     return "ok"
 
 if __name__ == "__main__":
